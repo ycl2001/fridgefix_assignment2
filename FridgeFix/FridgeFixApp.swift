06c136f0455c76beb215fdc9f40f5c67e844c265
Yen-Chun Liu <yen-chun.liu@student.uts.edu.au>
2026-09-04 10:21:40 +1000

    Initial Commit

---
+++ FridgeFix/FridgeFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeFixApp.swift
+//  FridgeFix
+//
+//  Created by Yen-Chun Liu on 4/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct FridgeFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FridgeFixTests/FridgeFixTests.swift
@@ -0,0 +1,19 @@
+//
+//  FridgeFixTests.swift
+//  FridgeFixTests
+//
+//  Created by Yen-Chun Liu on 4/9/2026.
+//
+
+import Testing
+@testable import FridgeFix
+
+struct FridgeFixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
